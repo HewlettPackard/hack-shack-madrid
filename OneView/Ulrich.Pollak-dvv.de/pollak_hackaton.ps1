83767bf945c77a7ba36255ec21233879cbae47bb
VincentBerger <vincent.berger@hpe.com>
2018-11-27 12:06:04 +0100

    Ulrich Pollak hack, good idea, done quickly, verified working

---
+++ OneView/Ulrich.Pollak-dvv.de/pollak_hackaton.ps1
@@ -0,0 +1,20 @@
+﻿#This Script shuts down all Server (powerState ne Off) on an emergency
+#Author: Ulrich Pollak
+
+Import-Module HPOneView.410
+
+#Connect-HPOVMgmt -Hostname 10.167.0.103 -UserName Administrator -Password password
+
+$arrServers = Get-HPOVServer
+
+Write-Host "Initiating Emergency Shutdown" -ForegroundColor Red
+
+
+foreach ($server in $arrServers) {
+
+  if($server.powerState -ne "Off") {
+    Stop-HPOVServer -InputObject $server  -Confirm:$false
+    Write-Host "Server Shutdown:" $server.name -ForegroundColor Yellow
+  }
+
+}
